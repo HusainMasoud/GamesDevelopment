f6d5482215717babd2ea7c5b774ff249ef7c811b
HusainMasoud <202301131@student.polytechnic.bh>
2026-03-19 07:32:56 +0300

    pushin
    push

---
+++ AngryBirds/Assets/Scripts/GameController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public SlingShooter SlingShooter;
+    public TrailController TrailController;
+    public List<Bird> Birds;
+    public List<Enemy> Enemies;
+    private Bird _shotBird;
+    public BoxCollider2D TapCollider;
+    public string levelName;
+    [SerializeField] private GameObject _panel;
+    [SerializeField] private Text _statusInfo;
+
+    private bool _isGameEnded = false;
+
+    void Start()
+    {
+        for (int i = 0; i < Birds.Count; i++)
+        {
+            Birds[i].OnBirdDestroyed += ChangeBird;
+            Birds[i].OnBirdShot += AssignTrail;
+        }
+
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            Enemies[i].OnEnemyDestroyed += CheckGameEnd;
+        }
+
+        TapCollider.enabled = false;
+        SlingShooter.InitiateBird(Birds[0]);
+        _shotBird = Birds[0];
+
+    }
+
+    public void ChangeBird()
+    {
+        TapCollider.enabled = false;
+
+        // If all enemies are dead, game ended = win
+        if (_isGameEnded)
+        {
+            string sceneName = SceneManager.GetActiveScene().name;  // Level1 / Level2 / Level3 etc.[web:71][web:69]
+
+            if (sceneName == "Level2")
+            {
+                _statusInfo.text = "You Win! Next Level?";
+            }
+            else
+            {
+                _statusInfo.text = "You Win!";
+            }
+
+            _panel.gameObject.SetActive(true);
+        }
+
+        // Remove the used bird
+        Birds.RemoveAt(0);
+
+        // If no birds left but enemies still alive -> lose
+        if (Birds.Count == 0 && Enemies.Count > 0)
+        {
+            _statusInfo.text = "You Lose!";
+            _panel.gameObject.SetActive(true);
+        }
+
+        // If there are still birds, load the next one into the slingshot
+        if (Birds.Count > 0)
+        {
+            SlingShooter.InitiateBird(Birds[0]);
+            _shotBird = Birds[0];
+        }
+        else
+        {
+            _isGameEnded = true;
+        }
+    }
+
+    public void CheckGameEnd(GameObject destroyedEnemy)
+    {
+        for (int i = 0; i < Enemies.Count; i++)
+        {
+            if (Enemies[i].gameObject == destroyedEnemy)
+            {
+                Enemies.RemoveAt(i);
+                break;
+            }
+        }
+
+        if (Enemies.Count == 0)
+        {
+            _isGameEnded = true;
+        }
+    }
+
+    public void AssignTrail(Bird bird)
+    {
+        TrailController.SetBird(bird);
+        StartCoroutine(TrailController.SpawnTrail());
+        TapCollider.enabled = true;
+    }
+
+    void OnMouseUp()
+    {
+        if (_shotBird != null)
+        {
+            _shotBird.OnTap();
+        }
+    }
+}
---
+++ AngryBirds/Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain : MonoBehaviour
+{
+    public void playAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void nextScene()
+    {
+        string current = SceneManager.GetActiveScene().name;
+
+        if (current == "Level1")
+            SceneManager.LoadScene("Level2");
+        else if (current == "Level2")
+            SceneManager.LoadScene("Level3");
+        else
+            SceneManager.LoadScene("Level1"); // loop back or change to MainMenu
+    }
+
+    public void lastScene()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void prevScene()
+    {
+        string current = SceneManager.GetActiveScene().name;
+
+        if (current == "Level3")
+            SceneManager.LoadScene("Level2");
+        else if (current == "Level2")
+            SceneManager.LoadScene("Level1");
+        else
+            SceneManager.LoadScene("Level1"); // loop back or change to MainMenu
+    }
+}
